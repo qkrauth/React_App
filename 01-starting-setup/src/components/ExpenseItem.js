f6ec881efb98755d84d3e5ba1bb7e819170376a0
Quinten Krauth <quintenkrauth19@gmail.com>
2022-11-08 17:01:34 -0500

    starting max app

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+
+//   const expenseDate = new Date(2021, 2, 28);
+//   const expenseTitle = "Car Insurance";
+//   const expenseAmount = 295.00;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
